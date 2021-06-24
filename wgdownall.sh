2a842c15e311f22f8d641c3deae0906b09133ca9
merkleID <luca@merkle.id>
2021-06-24 02:13:10 +0200

    up and down for all WGs

--- wgdownall.sh
+++ wgdownall.sh
@@ -6,7 +6,4 @@ list=$(ls -1 /etc/wireguard/*.conf)
 for nic in ${list[@]}
 	do
 	wg-quick down $nic
-	sleep 1
-	wg-quick up $nic
 done
-
--- wgupall.sh
+++ wgupall.sh
@@ -5,8 +5,6 @@ list=$(ls -1 /etc/wireguard/*.conf)
 
 for nic in ${list[@]}
 	do
-	wg-quick down $nic
-	sleep 1
 	wg-quick up $nic
 done
 
